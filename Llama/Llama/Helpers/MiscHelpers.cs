2f108794a36eaacdbe34ede0338f7e597ef04eda
neiljc07 <cuenzaneil@gmail.com>
2016-10-07 11:07:20 +0800

    Helpers

---
+++ Llama/Llama/Helpers/MiscHelpers.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+
+namespace Magsaysay.Commons.Helpers
+{
+    /// <summary>
+    /// Description of MiscHelpers.
+    /// </summary>
+    public static class MiscHelpers
+    {
+        public static long Timestamp()
+        {
+            return (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        public static string FormatDate(DateTime date, string format = "MMMM dd, yyyy")
+        {
+            return date.ToString(format);
+        }
+
+        public static double HeightToCM(decimal? feet = 0, decimal? inches = 0)
+        {
+            double feet_cm = (double) feet * 30.48;
+            double inches_cm = (double) inches * 2.54;
+
+            return feet_cm + inches_cm;
+        }
+
+        public static decimal[] ConvertHeightToFeetInches(decimal? cm = 0)
+        {
+            decimal[] cms = new decimal[2];
+
+            double c = (double) cm * 0.0328084;
+
+            cms[0] = Math.Truncate((decimal) c / 1);
+            cms[1] = (decimal) Math.Round((c  % 1) * 12, 0);
+
+            return cms;
+        }
+
+        public static string GenerateCode(int length = 50)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GetSetting(string key)
+        {
+            return ConfigurationManager.AppSettings[key].ToString();
+        }
+
+        public static decimal NullToZero(decimal? dec)
+        {
+            if (dec == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return (decimal) dec;
+            }
+        }
+
+        public static string NullToEmpty(string s)
+        {
+            if (s == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return s;
+            }
+        }
+    }
+}
---
+++ Llama/Llama/Helpers/StringHelpers.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace Llama.Helpers
+{
+    /// <summary>
+    /// Description of Helpers.
+    /// </summary>
+    public static class StringHelpers
+    {
+        public static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(s.ToLower());
+        }
+
+        public static string SpecialToUnderscore(string s)
+        {
+            return Regex.Replace(s, "[^a-zA-Z0-9_]+", "_");
+        }
+
+        public static string GetAlias(int count, string name)
+        {
+            if (count > 0)
+            {
+                return (StringHelpers.SpecialToUnderscore(name) + "_" + count.ToString()).ToLower();
+            }
+            else
+            {
+                return (StringHelpers.SpecialToUnderscore(name)).ToLower();
+            }
+        }
+
+        /// <summary>
+        /// Truncate a text based on the length
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static string Ellipsis(string text = "", int length = 300)
+        {
+            text = StripTagsRegex(text);
+            if (text.Length > length)
+            {
+                return text.Substring(0, length) + "...";
+            }
+            else
+            {
+                return text;
+            }
+        }
+
+        public static string StripTagsRegex(string source = "")
+        {
+            return Regex.Replace(source, "<.*?>", string.Empty);
+        }
+
+        public static string StripVowels(string word)
+        {
+            string vowels = "AEIOU";
+            return new string(word.ToUpper().Where(c => !vowels.Contains(c)).ToArray());
+        }
+
+        public static string StripSpaces(string word)
+        {
+            return word.Replace(" ", "").Trim();
+        }
+
+        public static string PageTitle(string name)
+        {
+            return UppercaseFirst("Magsaysay Careers | " + name.ToUpper().Replace("CONTROLLER", ""));
+        }
+
+        public static string URL(string url, string baseUrl)
+        {
+            Uri _base = new Uri(baseUrl);
+            return new Uri(_base, url).ToString();
+        }
+
+        public static string TagToEntities(string markup)
+        {
+            return markup.Replace("<", "&lt;").Replace(">", "&gt;");
+        }
+
+        public static string Upper(string s)
+        {
+            if (s != null)
+            {
+                return s.ToUpper();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
