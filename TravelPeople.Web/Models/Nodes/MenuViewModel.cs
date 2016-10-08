b265c96b95eeb90905f98900e9033954f77153f6
neiljc07 <cuenzaneil@gmail.com>
2016-10-08 11:08:57 +0800

    MenuViewModel.cs

---
+++ TravelPeople.Web/Models/Nodes/MenuViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TravelPeople.Commons.Interfaces;
+using TravelPeople.Commons.Objects;
+
+namespace TravelPeople.Web.Models.Nodes
+{
+    public class MenuViewModel : Menu
+    {
+
+        [Required(ErrorMessage = "This is required.")]
+        [DisplayName("Title")]
+        [UIHint("TextBox")]
+        [MaxLength(50)]
+        public string name
+        {
+            get;
+            set;
+        }
+
+        [DisplayName("Enabled")]
+        [UIHint("CheckBox")]
+        public bool enabled
+        {
+            get;
+
+            set;
+        }
+
+
+        [Required(ErrorMessage = "This is required.")]
+        [DisplayName("Header Text")]
+        [UIHint("TextBox")]
+        [MaxLength(50)]
+        public string header_text
+        {
+            get;
+            set;
+        }
+
+    }
+}
