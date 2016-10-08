b86a99eb4e7be019300dc1cad507525de2f110ae
johnvictordc <jvdelacruz@shinra.com.ph>
2016-10-08 10:30:08 +0800

    message

---
+++ TravelPeople.Commons/Utils/APIURL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelPeople.Commons.Utils
+{
+    public static class APIURL
+    {
+        public static string CONTENT_ALL = "api/content/getcontents";
+
+        public static string CONTENT_SINGLE = "api/content/getsingle";
+
+        public static string CONTENT_CREATE = "api/content/create";
+
+        public static string CONTENT_UPDATE = "api/content/update";
+
+        public static string CONTENT_DELETE = "api/content/delete";
+
+    }
+}
