d06aa97ebf824f31935c5d5c53b9b225c720c0b3
neiljc07 <cuenzaneil@gmail.com>
2016-10-18 09:58:57 +0800

    Passport Controller

---
+++ TravelPeople.Service/Controllers/PassportController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TravelPeople.Commons.Objects;
+using TravelPeople.DAL.Repositories;
+
+namespace TravelPeople.Service.Controllers
+{
+    public class PassportController : ApiController
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+               (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        private PassportRepository repo = new PassportRepository();
+
+        [AcceptVerbs("POST")]
+        [HttpPost]
+        public IHttpActionResult Create([FromBody] Passport model)
+        {
+            try
+            {
+                return Ok(repo.Insert(model));
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        [HttpPost]
+        public IHttpActionResult Update([FromBody] Passport model)
+        {
+            try
+            {
+                repo.Update(model);
+                return Ok(model.passportID);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AcceptVerbs("POST")]
+        [HttpPost]
+        public IHttpActionResult Delete([FromBody] long id)
+        {
+            try
+            {
+                return Ok(repo.Delete(id));
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AcceptVerbs("GET")]
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            try
+            {
+                return Ok(repo.GetAll());
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [AcceptVerbs("GET")]
+        [HttpGet]
+        public IHttpActionResult GetSingle(long id)
+        {
+            try
+            {
+                return Ok(repo.GetByID(id));
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error", ex);
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
