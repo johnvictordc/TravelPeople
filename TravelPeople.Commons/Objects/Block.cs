df7ac17d65301a3741e889dde68a70e0a6a32483
johnvictordc <jvdelacruz@shinra.com.ph>
2016-10-19 10:34:11 +0800

    Merge branch 'master' of https://github.com/johnvictordc/TravelPeople
    # Conflicts:
    #	TravelPeople.Web/Areas/CMS/Views/Shared/_SideMenu.cshtml
    #	TravelPeople.Web/Areas/OBT/Views/Booking/Index.cshtml
    #	TravelPeople.Web/Areas/OBT/Views/Shared/_SideMenu.cshtml
    #	TravelPeople.Web/Areas/OBT/Views/Traveler/Index.cshtml

---
+++ TravelPeople.Commons/Objects/Block.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Fujitsu AH530
+ * Date: 19/10/2016
+ * Time: 10:09 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TravelPeople.Commons.Objects
+{
+	/// <summary>
+	/// Description of Block.
+	/// </summary>
+	public class Block
+	{
+		public Block()
+		{
+		}
+	}
+}
